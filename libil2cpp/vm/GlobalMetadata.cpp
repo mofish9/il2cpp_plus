60322744721410e79203155fc455be4232c3df4b
mofish9 <mofish9@users.noreply.github.com>
2026-09-03 13:01:21 +0800

    feat: add Unity 2022 DHE metadata overlays

--- libil2cpp/vm/GlobalMetadata.cpp
+++ libil2cpp/vm/GlobalMetadata.cpp
@@ -1016,6 +1016,14 @@ static CustomAttributeIndex GetCustomAttributeIndex(const Il2CppImage* image, ui
 
 Il2CppMetadataCustomAttributeHandle il2cpp::vm::GlobalMetadata::GetCustomAttributeTypeToken(const Il2CppImage* image, uint32_t token)
 {
+    const Il2CppImage* dheSourceImage = NULL;
+    uint32_t dheSourceToken = 0;
+    if (hybridclr::metadata::MetadataModule::TryGetDheCustomAttributeSource(
+        image, token, dheSourceImage, dheSourceToken))
+    {
+        image = dheSourceImage;
+        token = dheSourceToken;
+    }
     if (hybridclr::metadata::IsInterpreterImage(image))
     {
         return hybridclr::metadata::MetadataModule::GetImage(image)->GetCustomAttributeTypeToken(token);
@@ -1049,6 +1057,14 @@ static il2cpp::metadata::CustomAttributeDataReader CreateCustomAttributeDataRead
 
 il2cpp::metadata::CustomAttributeDataReader il2cpp::vm::GlobalMetadata::GetCustomAttributeDataReader(const Il2CppImage* image, uint32_t token)
 {
+    const Il2CppImage* dheSourceImage = NULL;
+    uint32_t dheSourceToken = 0;
+    if (hybridclr::metadata::MetadataModule::TryGetDheCustomAttributeSource(
+        image, token, dheSourceImage, dheSourceToken))
+    {
+        image = dheSourceImage;
+        token = dheSourceToken;
+    }
     return CreateCustomAttributeDataReader(GetCustomAttributeTypeToken(image, token), image);
 }
 
